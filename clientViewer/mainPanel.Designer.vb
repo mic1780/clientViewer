3ae1e7f6ec66c4ae43bcf4820aea35e7b83ce92d
mic1780 <mic1780@users.noreply.github.com>
2014-07-21 15:36:32 -0400

    Initial commit

---
+++ clientViewer/mainPanel.Designer.vb
@@ -0,0 +1,240 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mainPanel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.clientTable = New System.Windows.Forms.DataGridView()
+        Me.ID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.socketNumber = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Active = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.isAdmin = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.isMonitor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.lastCommand = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.actionList = New System.Windows.Forms.ListBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.menuItem_File = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuItem_Connect = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuItem_Disconnect = New System.Windows.Forms.ToolStripMenuItem()
+        Me.menuItem_Help = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.bottomStatusText = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.bottomSeperater = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.connectionStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        CType(Me.clientTable, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'clientTable
+        '
+        Me.clientTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.clientTable.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ID, Me.socketNumber, Me.Active, Me.isAdmin, Me.isMonitor, Me.lastCommand})
+        Me.clientTable.Location = New System.Drawing.Point(12, 57)
+        Me.clientTable.Name = "clientTable"
+        Me.clientTable.RowTemplate.Height = 24
+        Me.clientTable.Size = New System.Drawing.Size(606, 325)
+        Me.clientTable.TabIndex = 0
+        '
+        'ID
+        '
+        Me.ID.HeaderText = "ID"
+        Me.ID.Name = "ID"
+        Me.ID.ReadOnly = True
+        Me.ID.Width = 40
+        '
+        'socketNumber
+        '
+        Me.socketNumber.HeaderText = "Socket #"
+        Me.socketNumber.Name = "socketNumber"
+        Me.socketNumber.ReadOnly = True
+        Me.socketNumber.Width = 70
+        '
+        'Active
+        '
+        Me.Active.HeaderText = "Is Active"
+        Me.Active.Name = "Active"
+        Me.Active.ReadOnly = True
+        Me.Active.Width = 70
+        '
+        'isAdmin
+        '
+        Me.isAdmin.HeaderText = "Is Admin"
+        Me.isAdmin.Name = "isAdmin"
+        Me.isAdmin.ReadOnly = True
+        Me.isAdmin.Width = 70
+        '
+        'isMonitor
+        '
+        Me.isMonitor.HeaderText = "Is Monitor"
+        Me.isMonitor.Name = "isMonitor"
+        Me.isMonitor.ReadOnly = True
+        Me.isMonitor.Width = 75
+        '
+        'lastCommand
+        '
+        Me.lastCommand.HeaderText = "Last Command"
+        Me.lastCommand.Name = "lastCommand"
+        Me.lastCommand.ReadOnly = True
+        Me.lastCommand.Width = 238
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Client Information"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(621, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(115, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Available Actions"
+        '
+        'actionList
+        '
+        Me.actionList.FormattingEnabled = True
+        Me.actionList.ItemHeight = 16
+        Me.actionList.Items.AddRange(New Object() {"Set Name", "Make Admin", "Make Monitor", "Disconnect", ""})
+        Me.actionList.Location = New System.Drawing.Point(624, 57)
+        Me.actionList.Name = "actionList"
+        Me.actionList.Size = New System.Drawing.Size(209, 84)
+        Me.actionList.TabIndex = 4
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.menuItem_File, Me.menuItem_Connect, Me.menuItem_Disconnect, Me.menuItem_Help})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(846, 28)
+        Me.MenuStrip1.TabIndex = 5
+        Me.MenuStrip1.Text = "menu"
+        '
+        'menuItem_File
+        '
+        Me.menuItem_File.Name = "menuItem_File"
+        Me.menuItem_File.Size = New System.Drawing.Size(44, 24)
+        Me.menuItem_File.Text = "File"
+        '
+        'menuItem_Connect
+        '
+        Me.menuItem_Connect.Name = "menuItem_Connect"
+        Me.menuItem_Connect.Size = New System.Drawing.Size(75, 24)
+        Me.menuItem_Connect.Text = "Connect"
+        '
+        'menuItem_Disconnect
+        '
+        Me.menuItem_Disconnect.Name = "menuItem_Disconnect"
+        Me.menuItem_Disconnect.Size = New System.Drawing.Size(94, 24)
+        Me.menuItem_Disconnect.Text = "Disconnect"
+        '
+        'menuItem_Help
+        '
+        Me.menuItem_Help.Name = "menuItem_Help"
+        Me.menuItem_Help.Size = New System.Drawing.Size(53, 24)
+        Me.menuItem_Help.Text = "Help"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.bottomStatusText, Me.bottomSeperater, Me.connectionStatus})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 418)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(846, 25)
+        Me.StatusStrip1.SizingGrip = False
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "bottomStatusBar"
+        '
+        'bottomStatusText
+        '
+        Me.bottomStatusText.Name = "bottomStatusText"
+        Me.bottomStatusText.Size = New System.Drawing.Size(195, 20)
+        Me.bottomStatusText.Text = "Click Connect to get started."
+        Me.bottomStatusText.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'bottomSeperater
+        '
+        Me.bottomSeperater.Name = "bottomSeperater"
+        Me.bottomSeperater.Size = New System.Drawing.Size(490, 20)
+        Me.bottomSeperater.Spring = True
+        '
+        'connectionStatus
+        '
+        Me.connectionStatus.Image = Global.clientViewer.My.Resources.Resources.plug_disconnect_slash
+        Me.connectionStatus.Name = "connectionStatus"
+        Me.connectionStatus.Size = New System.Drawing.Size(115, 20)
+        Me.connectionStatus.Text = "Disconnected"
+        Me.connectionStatus.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        '
+        'mainPanel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(846, 443)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.actionList)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.clientTable)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(864, 490)
+        Me.Name = "mainPanel"
+        Me.ShowIcon = False
+        Me.Text = "Activity Monitor"
+        CType(Me.clientTable, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents clientTable As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents actionList As System.Windows.Forms.ListBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents menuItem_File As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents menuItem_Connect As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents menuItem_Disconnect As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents menuItem_Help As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents socketNumber As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Active As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents isAdmin As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents isMonitor As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents lastCommand As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents bottomStatusText As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents connectionStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents bottomSeperater As System.Windows.Forms.ToolStripStatusLabel
+
+End Class
---
+++ clientViewer/mainPanel.vb
@@ -0,0 +1,331 @@
+﻿Imports System.Net.Sockets
+Public Class mainPanel
+    Dim msg As String
+    Dim lockObject As New Object()
+    Dim socketThread As Threading.Thread
+    Dim socket As Socket
+
+    Private Sub mainPanel_load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tableUpdate(New String() {"1", "1", "0", "0", "0", "set name User 1"})
+        tableUpdate(New String() {"2", "1", "1", "0", "0", "set name User 2"})
+    End Sub
+
+    Private Function Connect(ByVal host As String) As Socket
+        Dim dataReceived(1024) As [Byte]
+        Dim randVal As Integer
+        Dim octString As String = ""
+        Dim request As String = ""
+
+        Dim socket As Socket = New Socket(SocketType.Stream, ProtocolType.Tcp)
+        Try
+            socket.Connect(host, 1780)
+        Catch
+            SyncLock (lockObject)
+                bottomStatusText.Text = "Failed to connect to host at: " & host
+            End SyncLock
+            Console.WriteLine("ERROR: Failed to connect to host. Exiting Connect function...")
+            Return Nothing
+            Exit Function
+        End Try
+
+        If socket.Connected = False Then
+            Return Nothing
+        End If
+        Dim stream As New NetworkStream(socket)
+        Dim reader As New IO.StreamReader(stream)
+        Dim writer As New IO.StreamWriter(stream)
+
+        For i As Int16 = 1 To 16 Step 1
+            Randomize()
+            randVal = CInt(Math.Floor((256 * Rnd()) + 1))
+            octString &= Oct(randVal)
+        Next
+
+        request &= "GET ws://" & host & ":1780/" & vbCrLf & _
+                   "Connection: Upgrade" & vbCrLf & _
+                   "Upgrade: websocket" & vbCrLf & _
+                   "Sec-WebSocket-Key: " & getSHA1Hash(octString) & vbCrLf & _
+                   "Sec-WebSocket-Version: 13" & vbCrLf & vbCrLf
+
+        writer.Write(request)
+        writer.Flush()
+        While (Not (reader.Peek() = Nothing Or reader.Peek() = -1))
+            Console.WriteLine("'" & reader.Peek() & "'")
+            Console.WriteLine(reader.ReadLine())
+        End While
+
+        connectionStatus.Text = "Connected"
+
+        msg = "set name monitor"
+        SendMessage2(socket, msg, msg.Length)
+        msg = "updateTable 1 1 4 1 1 set name monitor"
+        SendMessage2(socket, msg, msg.Length)
+        'SendMessage2(socket, "set name monitor", "set name monitor".Length)
+        Return socket
+
+    End Function
+
+    Private Sub socketActive(socket As Socket)
+
+        'Set up variables for our streams
+        Dim dataReceived(1024) As Byte
+        Dim bytesReceived As Integer
+
+        Dim message As String
+        Dim messageSplit As String()
+
+        While (socket.Connected)
+            bytesReceived = socket.Receive(dataReceived, dataReceived.Length, 0)
+
+            'If we received -1 bytes or a closing byte, close the socket
+            If (bytesReceived <= 0 OrElse dataReceived(0) = &H88) Then
+                Console.WriteLine("Close byte was received. Closing socket.")
+                socket.Disconnect(False)
+                Exit While
+            End If
+            message = Nothing
+            message = decodeMessage(dataReceived, bytesReceived)
+            'Console.WriteLine("Message: " & message)
+            'we dont get here
+            Console.WriteLine("We got here")
+            messageSplit = Split(message, " ", 2)
+
+            If (messageSplit(0).ToLower() = "updatetable") Then
+                Console.WriteLine("We got here")
+                ' = New String() {messageSplit(0), messageSplit(1), messageSplit(2), messageSplit(3), messageSplit(4), messageSplit(5)}
+                Dim tableCells As String() = Split(messageSplit(1), " ", 6)
+                'messageSplit = Split(messageSplit(1), " ", 6)
+                If Me.clientTable.InvokeRequired Then
+                    Me.clientTable.Invoke(New Action(Sub() tableUpdate(tableCells)))
+                Else
+                    tableUpdate(tableCells)
+                End If
+                'SyncLock (lockObject)
+
+                'End SyncLock
+            End If
+            Console.WriteLine("Message: '" & message & "'")
+
+        End While
+
+        connectionStatus.Text = "Disconnected"
+
+    End Sub
+
+    Private Function decodeMessage(ByVal data() As Byte, ByVal len As Integer) As String
+        'Initialize counter variables
+        Dim i, j As Integer
+
+        'Initialize some variable to help decode the message
+        Dim maskIndex As Integer
+        Dim mask(4) As Byte
+
+        Dim decodedBytes() As Byte
+
+        'Get our mask index position
+        If ((data(1) & CByte(&H7F)) = 126) Then
+            maskIndex = 4
+        ElseIf ((data(1) & CByte(&H7F)) = 127) Then
+            maskIndex = 10
+        Else
+            maskIndex = 2
+        End If
+
+        ReDim decodedBytes(len - maskIndex - 4)
+
+        For i = 0 To 3
+            mask(i) = data(maskIndex + i)
+        Next
+
+        j = maskIndex + 4
+
+        For i = 0 To len - maskIndex - 4
+            'Console.WriteLine(Convert.ToByte(data(j)) Xor mask(i Mod 4))
+            decodedBytes(i) = data(j) Xor mask(i Mod 4)
+            j += 1
+        Next
+
+        If (data(1) = len - 4) Then
+            'Console.WriteLine("Data(1) = len: '" & System.Text.Encoding.UTF8.GetString(data, maskIndex, len - 4) & "'")
+            msg = System.Text.Encoding.UTF8.GetString(data, maskIndex, len - 4)
+        Else
+            'Console.WriteLine(System.Text.Encoding.UTF8.GetString(decodedBytes))
+            msg = System.Text.Encoding.UTF8.GetString(decodedBytes)
+        End If
+
+        Return msg
+    End Function
+
+    Private Sub tableUpdate(ByRef rowData As String())
+
+        If rowData.Count > 0 Then
+
+            If clientTable.RowCount > 1 Then
+
+                For Each row In clientTable.Rows
+
+                    If row.Index = clientTable.Rows.Count - 1 Then
+                        Continue For
+                    End If
+                    For Each cell In row.Cells
+                        Console.Write(clientTable.Rows(row.Index).Cells(cell.ColumnIndex).Value)
+                        Console.Write(" ")
+                    Next
+                    Console.Write(vbCrLf)
+                    'Console.WriteLine(If(row.Cells(0).Value = 0, "0", row.Cells(0).Value()))
+                Next
+
+            End If
+
+            clientTable.Rows.Add(rowData)
+        End If
+
+    End Sub
+
+    Function getSHA1Hash(ByVal strToHash As String) As String
+        Dim sha1Obj As New Security.Cryptography.SHA1CryptoServiceProvider
+        Dim bytesToHash() As Byte = System.Text.Encoding.ASCII.GetBytes(strToHash)
+        Dim result As String
+
+        bytesToHash = sha1Obj.ComputeHash(bytesToHash)
+        result = Convert.ToBase64String(bytesToHash)
+
+        Return result
+    End Function
+
+    Sub SendMessage2(ByVal socket As Socket, ByVal message As String, ByVal len As Integer)
+        Dim frameCount As Integer
+        Dim len16 As UInt16
+        Dim reply(len + 8) As [Byte]
+        Dim frame(10) As [Byte]
+        Dim maskingBytes(4) As [Byte]
+
+        frame(0) = CByte(&H81)
+
+        If (len <= 125) Then
+            frame(1) = CByte(len + 128)
+            frameCount = 2
+        ElseIf (len >= 126 AndAlso len <= 65535) Then
+            frame(1) = CByte(126 + 128)
+            len16 = Convert.ToUInt16(len)
+            frame(2) = CByte(BitConverter.GetBytes(len16).GetValue(0))
+            frame(3) = CByte(BitConverter.GetBytes(len16).GetValue(1))
+            frameCount = 4
+        Else
+            frame(1) = CByte(127 + 128)
+            'Odds are we are not ever going to get here. but here is some code
+            'frame(2) = CByte(((len >> 56) And CByte(255)))
+            'frame(3) = CByte(((len >> 48) And CByte(255)))
+            'frame(4) = CByte(((len >> 40) And CByte(255)))
+            'frame(5) = CByte(((len >> 32) And CByte(255)))
+            'frame(6) = CByte(((len >> 24) And CByte(255)))
+            'frame(7) = CByte(((len >> 16) And CByte(255)))
+            'frame(8) = CByte(((len >> 8) And CByte(255)))
+            'frame(9) = CByte((len) And CByte(255))
+            frameCount = 10
+        End If
+
+        For i As Integer = 0 To frameCount - 1
+            reply(i) = Convert.ToByte(frame(i))
+        Next
+
+        For i As Integer = 0 To 3
+            Randomize()
+            maskingBytes(i) = CByte(Int((128 * Rnd()) + 1) Mod 56 * 2)
+            reply(frameCount + i) = maskingBytes(i)
+        Next
+
+        For i As Integer = 0 To len
+            If (Not i = len) Then
+                reply(frameCount + i + 4) = Convert.ToByte(message.Chars(i)) Xor maskingBytes(i Mod 4)
+            Else
+                reply(frameCount + i + 4) = CByte(0) Xor maskingBytes(i Mod 4)
+            End If
+        Next
+
+
+        If (socket.Send(reply) <= 0) Then
+            Console.WriteLine("WE ARE NOT WRITING!!")
+        End If
+
+    End Sub
+
+    Sub SendMessage(sck As Socket, message As String)
+        Dim rawData = System.Text.Encoding.UTF8.GetBytes(message)
+        Dim decoded = System.Text.Encoding.UTF8.GetChars(rawData)
+        Console.WriteLine(decoded)
+        Dim frameCount = 0
+        Dim frame(10) As Byte
+        frame(0) = CByte(129)
+
+        If rawData.Length <= 125 Then
+            frame(1) = CByte(rawData.Length + 1)
+            frameCount = 2
+        ElseIf rawData.Length >= 126 AndAlso rawData.Length <= 65535 Then
+            frame(1) = CByte(126)
+            Dim len = CByte(rawData.Length)
+            frame(2) = CByte(((len >> 8) And CByte(255)))
+            frame(3) = CByte((len) And CByte(255))
+            frameCount = 4
+        Else
+            frame(1) = CByte(127)
+            Dim len = CByte(rawData.Length + 1)
+            frame(2) = CByte(((len >> 56) And CByte(255)))
+            frame(3) = CByte(((len >> 48) And CByte(255)))
+            frame(4) = CByte(((len >> 40) And CByte(255)))
+            frame(5) = CByte(((len >> 32) And CByte(255)))
+            frame(6) = CByte(((len >> 24) And CByte(255)))
+            frame(7) = CByte(((len >> 16) And CByte(255)))
+            frame(8) = CByte(((len >> 8) And CByte(255)))
+            frame(9) = CByte((len) And CByte(255))
+            frameCount = 10
+        End If
+        Dim bLength = frameCount + rawData.Length
+        Console.WriteLine(frameCount)
+        Console.WriteLine(rawData.Length)
+        Dim reply(bLength) As Byte
+
+        Dim bLim = 0
+        For i = 0 To frameCount - 1
+            reply(bLim) = frame(i)
+            bLim += 1
+        Next
+
+        For i = 0 To rawData.Length - 1
+            reply(bLim) = rawData(i)
+            bLim += 1
+        Next
+
+        sck.Send(reply)
+    End Sub
+
+    Private Sub Connect_Click(sender As Object, e As EventArgs) Handles menuItem_Connect.Click
+        Dim msg As String
+        If (Not (socket Is Nothing OrElse socket.Connected = False)) Then
+            Exit Sub
+        End If
+        socket = Connect("127.0.0.1")
+        If (socket IsNot Nothing AndAlso socket.Connected) Then
+            socketThread = New Threading.Thread(AddressOf Me.socketActive)
+
+            socketThread.Name = "socketThread"
+            socketThread.Start(socket)
+        Else
+            socket = Nothing
+        End If
+    End Sub
+
+    Private Sub connectionStatusChange(sender As Object, e As EventArgs) Handles connectionStatus.TextChanged
+        Dim text As String = connectionStatus.Text.ToLower()
+        If (text = "connected") Then
+            connectionStatus.Image = My.Resources.plug_connect
+        ElseIf (text = "disconnected") Then
+            connectionStatus.Image = My.Resources.plug_disconnect_slash
+        End If
+    End Sub
+
+    Private Sub Disconnect_Click(sender As Object, e As EventArgs) Handles menuItem_Disconnect.Click
+        socket.Disconnect(False)
+        socket = Nothing
+    End Sub
+End Class
